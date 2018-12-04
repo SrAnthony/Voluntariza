348238c2ab99f968c07ee8d83445dd6c3381dd2a
Anthony Nadaletti <anthony.nadaletti@gmail.com>
2018-12-04 19:14:58 -0200

    Annotate

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: companies
+#
+#  id         :bigint(8)        not null, primary key
+#  name       :string(255)
+#  phone      :string(255)
+#  cnpj       :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Company < ApplicationRecord
 end
--- test/models/volunteer_test.rb
+++ test/models/volunteer_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: volunteers
+#
+#  id         :bigint(8)        not null, primary key
+#  name       :string(255)
+#  phone      :string(255)
+#  cpf        :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class VolunteerTest < ActiveSupport::TestCase
