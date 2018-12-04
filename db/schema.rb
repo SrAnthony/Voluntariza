fa4a4f8a81aea9cec0a972a24c43202a6cd89a81
Anthony Nadaletti <anthony.nadaletti@gmail.com>
2018-12-04 19:36:27 -0200

    Adiciona role em user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_04_212209) do
+ActiveRecord::Schema.define(version: 2018_12_04_213504) do
 
   create_table "companies", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2018_12_04_212209) do
     t.string "cnpj"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_companies_on_user_id"
   end
 
   create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -39,6 +41,7 @@ ActiveRecord::Schema.define(version: 2018_12_04_212209) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -51,5 +54,6 @@ ActiveRecord::Schema.define(version: 2018_12_04_212209) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "companies", "users"
   add_foreign_key "events", "companies"
 end
